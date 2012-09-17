fb56cefe8e6c839105e861130457526bc54429df
GTL <admin@adminmatoMac-Pro-2.local>
2012-09-17 10:34:57 +0800

    Initial Commit

---
+++ testOpenTencent/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  testOpenTencent
+//
+//  Created by admin on 12-9-17.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ testOpenTencent/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testOpenTencent
+//
+//  Created by admin on 12-9-17.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
